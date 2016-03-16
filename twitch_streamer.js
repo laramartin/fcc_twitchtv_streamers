8db34ea00490f33484e3a68af6b133011db38537
Lara Martin <larikimc@gmail.com>
2016-03-16 12:47:22 +0100

    cleaned code

--- twitch_streamer.js
+++ twitch_streamer.js
@@ -1,37 +1,28 @@
 $(document).ready(function() {
-  //streamer(urlAccount);
-
-
   var channels = ["freecodecamp", "ESL_SC2", "OgamingHS", "storbeck", "terakilobyte", "habathcx","RobotCaleb",
   "thomasballinger","noobs2ninjas","beohoff", "brunofin", "comster404"]
 
-
   function getUrlData(channel){
     return "https://api.twitch.tv/kraken/streams/" + channel + "?callback=?";
-  }
+  };
 
   function getUrlChannel(channel){
     return "https://www.twitch.tv/" + channel;
-  }
-
+  };
 
   function streamerStatus(name) {
     $.getJSON(getUrlData(name), function(data) {
-      console.log(data);
       var statusChannel = "";
       var offlineBackground = "#DBCDE6";
 
       if (data.stream == null) {
         if (data.status == 422){
-          console.log("error 422: " + data.status);
           statusChannel = "Account closed";
 
         }else {
-          console.log("There's no streaming, sorry!");
           statusChannel = "Offline";
         }
       } else {
-        console.log("NOW LIVE!!");
         statusChannel = data.stream.game;
         offlineBackground = "LightGreen";
       }
@@ -39,38 +30,20 @@ $(document).ready(function() {
     });
   };
 
-
   function displayData(channelsList) {
-
-
     for (var i = 0; i < channelsList.length; i++){
       var name = channelsList[i];
       var urlDataAccount = getUrlData(name);
       streamerStatus(name);
-
       var urlChannel = getUrlChannel(name);
-
-          console.log("return of getUrlAccount: " + getUrlData(channels[i]));
-          console.log("url get data Account: " + urlDataAccount);
-          console.log("url channel: " + urlChannel);
-          //console.log("status: " + statusChannel);
-          console.log("==========");
     };
-
-
   };
 
   function showChannel(urlChannel, name, status, offlineBackground){
-
-    console.log("this is the color: " + offlineBackground);
-
     var html = "<a href='" + urlChannel + "' target='_blank'>" +
     "<div class='results row container-fluid' style='background-color:" + offlineBackground + "'>" +
     "<div class='col-xs-6 col-sm-4'>" + "<b>" + name + "</b>" + "</div>" +
     "<div class='col-xs-12 col-sm-8'>" + "<em>" + status + "</em></div>" +
-
-    //"<br> url account: " + urlDataAccount +
-    //"<br> status: " + statusChannel +
     "</p></div></a>";
 
     $(".resultsList").append(html);
@@ -78,4 +51,11 @@ $(document).ready(function() {
 
   displayData(channels);
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-75149386-1', 'auto');
+  ga('send', 'pageview');
 });
